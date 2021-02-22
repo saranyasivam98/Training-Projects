fd461971bcc18fec49e59b816b43479a9a7d7dfa
Saranya S <69453063+saranyasivam98@users.noreply.github.com>
2021-02-22 12:06:43 +0530

    Add files via upload

---
+++ Assignment2/stylecheck.bat
@@ -0,0 +1,6 @@
+echo
+echo RUNNING LINTER ON APPLICATION CODE ...
+echo
+
+
+"C:\Users\saran\Documents\GyanData\Github_files\Part2\venv\Scripts\pylint.exe" check_csv.py --rcfile=pylintrc.ini > linter_output.txt
